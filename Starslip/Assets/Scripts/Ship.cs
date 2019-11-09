6f83679c0519e852402f0510858eedad007cdf24
Benjamin Shutt <shuttben@gmail.com>
2019-11-08 22:31:35 -0800

    Basic ship movement

---
+++ Starslip/Assets/Scripts/Ship.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float Speed = 18;
+
+    private InputMaster inputMaster;
+    private Vector2 direction;
+
+    protected void Awake()
+    {
+        inputMaster = new InputMaster();
+        inputMaster.Player.Move.performed += ctx => UpdateDirection(ctx.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+
+        inputMaster.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputMaster.Disable();
+    }
+
+    private void UpdateDirection(Vector2 dir)
+    {
+        direction = dir;
+    }
+
+    private void Update()
+    {
+        Move();
+        ClampPosition();
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction * Speed * Time.deltaTime);
+    }
+
+    private void ClampPosition()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
