3345ff23ade3402a011f73a1baeba5169191b9a0
Robert Coty <robert.coty.jr@gmail.com>
2019-11-09 12:57:12 -0800

    asteroid prefab and rotation script

---
+++ Starslip/Assets/Scripts/asteroid/asteriod.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteriod : MonoBehaviour
+{
+    Transform myTrans;
+    Vector3 rotation;
+
+    public float rotationOffset = 50f;
+
+    private void Awake()
+    {
+        myTrans = transform; 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Random rotation (speed and dir)
+        rotation.x = Random.Range(-rotationOffset, rotationOffset);
+        rotation.y = Random.Range(-rotationOffset, rotationOffset);
+        rotation.z = Random.Range(-rotationOffset, rotationOffset);
+
+        Debug.Log(rotation);
+
+        // set random size
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate
+        myTrans.Rotate(rotation * Time.deltaTime);
+
+    }
+}
