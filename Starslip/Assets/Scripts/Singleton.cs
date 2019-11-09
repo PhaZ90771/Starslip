f81c4113163d87f276e752eb4eafa1866d038f89
Benjamin Shutt <shuttben@gmail.com>
2019-11-08 20:04:12 -0800

    Init some folders

---
+++ Starslip/Assets/Scripts/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T Instance { get; private set; }
+    public static bool IsInitialized { get { return Instance != null; } }
+
+    protected virtual void Awake()
+    {
+        if (IsInitialized)
+        {
+            Debug.LogError("[Singleton] Trying to initialize a second instance of a singleton class.");
+        }
+        else
+        {
+            Instance = (T)this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+}
