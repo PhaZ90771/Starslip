4dedda3df49b3c483728e8295379c526a089c22d
Robert Coty <robert.coty.jr@gmail.com>
2019-11-09 17:18:41 -0800

    asteroids

--- Starslip/Assets/Scripts/alien/Alien.cs
+++ Starslip/Assets/Scripts/alien/Alien.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour
 {
+    Transform mainCam;
     Transform myTrans;
     Vector3 oldPosition;
     Vector3 targetPosition;
 
     public float speed = 1f;
-    float speedCompensator = 0f;
     public int health = 100;
     public int damage = 10;
     public float flightPerimeter = 10f;
 
     float lerpTime;
 
+    float timeStamp;
+
+
+
+
+
+
 
     void Awake()
     {
         myTrans = transform;
+        mainCam = Camera.main.transform;
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        timeStamp = Time.time;
+        SetStartPos();
         oldPosition = myTrans.position;
-        //set random target.x, square it, subtract
-        // from U and square root to get target.y
         SetTargetPos();
 
     }
@@ -35,14 +44,17 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(myTrans.position, targetPosition) <= 1f)
+        /*if (timeStamp+4f<=Time.time)
         {
             SetTargetPos();
-        }
+            timeStamp = Time.time;
+        }*/
 
         //transform.Translate(targetPosition * speed * Time.deltaTime);
-        lerpTime += (speed+speedCompensator) / 10 * Time.deltaTime;
-        myTrans.position = Vector3.Lerp(oldPosition,new Vector3(targetPosition.x,targetPosition.y,targetPosition.z), lerpTime); //TODO: set targetposition.z position to camera's z pos
+        lerpTime += speed / 10 * Time.deltaTime;
+        //transform.localRotation = mainCam.rotation;
+        //myTrans.position = Vector3.Lerp(oldPosition+mainCam.position,targetPosition+mainCam.position, lerpTime);
+        SetStartPos();
     }
 
     public void TakeDamage(int damage)
@@ -54,20 +66,18 @@ public class Alien : MonoBehaviour
         }
     }
 
-    public void SetTargetPos()
+    void SetTargetPos()
     {
         oldPosition = myTrans.position;
         lerpTime = 0f;
-        float targetX = Random.Range(-flightPerimeter, flightPerimeter);
-        float targetY = Random.Range(-flightPerimeter, flightPerimeter);
-        targetPosition = new Vector3(targetX, targetY, myTrans.position.z);
-        if (Mathf.Abs((oldPosition.x+oldPosition.y)-(targetPosition.x+targetPosition.y))<=8f)
-        {
-            speedCompensator = 5;
-        }
-        else
-        {
-            speedCompensator = 0;
-        }
+        float targetXYZ = Random.Range(-flightPerimeter, flightPerimeter);
+        targetPosition = new Vector3(targetXYZ, targetXYZ, targetXYZ);
+        
+    }
+
+    void SetStartPos()
+    {
+        myTrans.position =  mainCam.position+(Vector3.forward*10);//new Vector3(mainCam.position.x, mainCam.position.y, mainCam.position.z + 20);
+        myTrans.rotation = mainCam.rotation;
     }
 }
