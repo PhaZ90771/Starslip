5278596ea9f064c2a17471fccfa949ffa945444e
Benjamin Shutt <shuttben@gmail.com>
2019-11-10 16:01:25 -0800

    Fixed and udpated bullet explosion

--- Starslip/Assets/Scripts/Bullet.cs
+++ Starslip/Assets/Scripts/Bullet.cs
@@ -6,6 +6,8 @@ public class Bullet : MonoBehaviour
     public int Damage = 5;
     public GameObject Explosion;
 
+    private bool explosion = false;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Asteroid")
@@ -14,20 +16,23 @@ public class Bullet : MonoBehaviour
             if (asteroid)
                 asteroid.TakeDamage(Damage);
             LifeTime = 0f;
+            explosion = true;
         }
         else if (collision.gameObject.tag == "Alien")
         {
             collision.gameObject.GetComponent<Alien>().TakeDamage(Damage);
             LifeTime = 0f;
+            explosion = true;
         }
     }
 
     private void Update()
     {
         LifeTime -= Time.deltaTime;
-        if (LifeTime <= 0f)
+        if (LifeTime <= 0f && explosion)
         {
-            Instantiate(Explosion, transform.position, new Quaternion(), null);
+            explosion = false;
+            var exp = Instantiate(Explosion, transform.position, transform.rotation, null);
             Destroy(gameObject);
         }
     }
